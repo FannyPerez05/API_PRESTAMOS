0714d3e20a98927f2b3d9e4327e8d3b9defbfcc9
Fanny Perez <fanny10araceli@gmail.com>
2026-07-17 08:34:50 -0600

    cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const pool = require('./db');
 
 const app = express();
 
+
 app.use(express.json());
+
+app.use(cors());
 
 // Obtener todos los préstamos
 app.get('/prestamos', async (req, res) => {
